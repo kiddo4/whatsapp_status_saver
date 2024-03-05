cc40f9bb523aefbee11f0a2d0be5fec67efd7578
Olanrewaju taiwo <107349861+kiddo4@users.noreply.github.com>
2024-03-05 01:42:20 +0100

    updates

--- lib/Ui/Screens/splash_screen.dart
+++ lib/Ui/Screens/splash_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:status_saver/Ui/Screens/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    void navigate () {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    navigate();
+  }
+
+  @override
+  void navigate () {
       Future.delayed(const Duration(seconds: 3), () {
         Navigator.pushAndRemoveUntil(
           context, 
@@ -18,6 +29,10 @@ class SplashScreen extends StatelessWidget {
         
       });
     }
+
+  @override
+  Widget build(BuildContext context) {
+    
     return const Scaffold(
       body: Center(
         child: Text('My Saver'),
